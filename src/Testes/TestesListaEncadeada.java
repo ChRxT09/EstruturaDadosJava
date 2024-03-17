2d34e0c9bdc4296866638264c3f9cb25ef10af28
ChRxT09 <gugu.olichrist@gmail.com>
2024-03-16 21:12:39 -0300

    general fixes

--- src/Testes/TestesListaEncadeada.java
+++ src/Testes/TestesListaEncadeada.java
@@ -17,7 +17,7 @@ class TestesListaEncadeada {
 		assertNull(lista.getPrim());
 		assertNull(lista.getUlt());
 		
-		System.out.println(lista.toString());
+		// System.out.println(lista.toString());
 		
 		
 		Item item0 = new Item(0, "teste");
@@ -31,13 +31,11 @@ class TestesListaEncadeada {
 		No<Item> no3 = new No<Item>(item3);
 		
 		lista.inserirInicio(no0);
-		System.out.println(lista.toString());
 		
 		assertFalse(lista.eVazia());
 		assertEquals(no0, lista.getPrim());
 		
 		lista.inserirFim(no3);
-		// System.out.println(lista.toString());
 		
 		assertEquals(no3, lista.getUlt());
 		assertEquals(2, lista.getQuant());
@@ -55,9 +53,32 @@ class TestesListaEncadeada {
 		assertEquals(4, lista.getQuant());
 		
 		assertEquals(no0, lista.removerPrimeiro());
+		// System.out.println(lista.toString());
+
 		assertEquals(no3, lista.removerUlt());
-		
-		
+		// System.out.println(lista.toString());
+
+		assertEquals(no1, lista.getPrim());
+		assertEquals(no2, lista.getUlt());
+
+		ListaEncadeada lista2 = new ListaEncadeada();
+
+		No<Item> no4 = new No<Item>(item0);
+		No<Item> no5 = new No<Item>(item1);
+		No<Item> no6 = new No<Item>(item2);
+		No<Item> no7 = new No<Item>(item3);
+
+
+		lista2.inserirKesimaPosicao(no4, 0);
+		lista2.inserirInicio(no5);
+		lista2.inserirFim(no6);
+		lista2.inserirKesimaPosicao(no7, 2);
+
+		lista.concatenar(lista2);
+
+		assertEquals(6, lista.getQuant());
+
+
 	}
 
 }
